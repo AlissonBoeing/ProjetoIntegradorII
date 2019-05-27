937256d8101aa0d87e72c9799202dd82c21f30b9
Rafael Teles <rafatespindola@gmail.com>
2019-05-27 18:41:59 -0400

    Versao Teste no robo

--- Treasure.py
+++ Treasure.py
@@ -1,8 +1,8 @@
 class Treasure:
-    #Exemplo do que chega em lcacas  ->  "cacas,1:1;2:3;5:2;6:6;4:3;2:1"
+    #Exemplo do que chega em lcacas  ->  "1:1;2:3;5:2;6:6;4:3;2:1"
     def __init__(self, l):
-        self.l1 = list(l[6:].split(';'))
-        self.lcacas = l[6:]
+        self.l1 = list(l.split(';'))
+        self.lcacas = l
         # pega só a partir do setimo caracter e fica 1:1;2:3;5:2;6:6;4:3;2:1
         # ['1:1', '2:3', '5:2', '6:6', '4:3', '2:1']
 
